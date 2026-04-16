c0cfb9d5dba055f847b842cc5ac918375ae8cac9
Dongri Pavan Kumar <pavankumardongri@gmail.com>
2026-04-16 19:03:16 +0530

    added db connection checks

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,8 +3,20 @@ import { Pool } from "pg";
 import { env } from "../config/env";
 import * as schema from "./schema";
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: env.DATABASE_URL
 });
 
 export const db = drizzle(pool, { schema });
+
+export async function checkDbConnection(): Promise<void> {
+  if (!env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is missing");
+  }
+  const client = await pool.connect();
+  try {
+    await client.query("SELECT 1 as ok");
+  } finally {
+    client.release();
+  }
+}
